7b6dfd611ecffbe55864a7b1d060a35fdc26aed8
Zhabskyi <poltavagaz@gmail.com>
2019-08-24 19:45:27 -0600

    customizing bars to handle position of values and spacing between them

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,56 +1,68 @@
-const data = [10, 20, 30, 40, 50, 60, 70, 70];
+const data = [55, 80, 11, 40, 50, 60, 20, 70];
 
 const options = {
-  width: '300',
-  height: '300'
+  axisHeight: '500',
+  colors: ['Maroon', 'Blue', 'Salmon', 'DarkSeaGreen', 'Purple', 'DarkSlateGray', 'Crimson'],
+  valuePosition: 'center',
+  barSpacing: '40',
+  barWidth: '30'
 }
 
 const element = document.querySelector('#root');
+const header = document.querySelector('.header');
 
-
-const axisHeight = 500;
 const axisWidth = defineWidth(data);
+let textPosition = 0;
 
 
 
 
 function drawBarChart(data, options, element) {
-  renderCharts(data);
+  renderCharts(data, options);
 }
 
 function renderAxis(element) {
  let axis =  $('<div></div>')
     .addClass('axis')
-    .css('width', `${axisWidth}px`)
+    .css({
+      'width': `${axisWidth}px`,
+      'height': `${options.axisHeight}px`
+    })
     .appendTo(element);
     return axis;
 }
 
-function renderCharts(data) {
+function renderCharts(data, options) {
   const axis = renderAxis(element);
   const maxNumber = Math.max(...data);
-  const minNumber = Math.min(...data);
 
-  scaleNumber = (axisHeight/maxNumber)*0.95;
+  scaleNumber = (options.axisHeight/maxNumber)*0.95;
 
   const scaledArr = scaleArray(data, scaleNumber);
 
   scaledArr.map( (el, i) => {
     $('<div></div>')
       .addClass('bar')
-      .css('height',`${el}px`)
+      .css({
+        'height':`${el}px`,
+         'justify-content':
+          `${options.valuePosition}` === 'top' ? 'flex-start' :
+          `${options.valuePosition}` === 'center' ? 'space-evenly' :
+          `${options.valuePosition}` === 'bottom' ? 'flex-end' : 'flex-end',
+          'background-color': `${options.colors[i]}`
+      })
+      .text(data[i])
       .appendTo(axis);
-  })
-  console.log(maxNumber);
-  console.log(minNumber);
+  });
 }
 
 function defineWidth(data) {
-  return (data.length + 1) * 80;
+  return (data.length + 1) * (Number(options.barSpacing) + Number(options.barWidth));
 }
 
 function scaleArray(arr, scaleNumber) {
   return arr.map(el => el*scaleNumber)
 }
+
 
 drawBarChart(data, options, element);
